7ecc63d2e0d82fd7e284dc7670bdd5fdd66a49ad
Andrés Manelli <andresmanelli@gmail.com>
2018-09-22 12:28:36 +0200

    Update README and setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 function install {
-    rm -f ~/.vimrc
+    rm -f ~/.vimrc ~/.config/nvim/init.vim
     # Download vim.plug
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     # Create symlink to vimrc
     ln -s  ~/.vim/vimrc ~/.vimrc
+    # Create symlink to init.vim
+    mkdir -p ~/.config/nvim && ln -s ~/.vim/init.vim ~/.config/nvim/init.vim
 }
 
 install
