dc0d941a03ab461cf65041e1738b9b7afb0fca08
Fernando Cladera <fcladera@fcladera.com>
2022-08-09 23:55:33 -0400

    A way more compact basic.lua file

--- lua/fclad/basic.lua
+++ lua/fclad/basic.lua
@@ -2,76 +2,29 @@
 -- Basic configurations
 ----------------------------------------
 
--- Prefer Unix based file format, only LF at end line
-vim.o.fileformats = 'unix,dos'
-
--- Do smart autoindenting when starting a new line
-vim.o.smartindent = true
-
--- Ignore case in search patterns unless upercase found
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Avoid numbers as 007 to be read as octal numbers
--- FIXME
--- vim.o.nrformats:remove('octal')
-
--- Timeout for escape sequences
-vim.o.ttimeoutlen = 0
-
--- Show (partial) command in the last line of the screen
-vim.o.showcmd = true
-
--- Number of lines above and below the cursor
-vim.o.scrolloff = 1
-
---  Automatically reload files changed outside vim
-vim.o.autoread = true
-
--- Long command history
-vim.o.history = 1000
-
--- Split in a more natural way (right instead of below
-vim.o.splitright = true
-
--- Show where the pattern matches while typing a search command
-vim.o.incsearch = true
-
--- Highlight matches
-vim.o.hlsearch = true
-
--- Match brackets briefly
-vim.o.showmatch = true
-
--- Avoid modelines exploits
-vim.o.modelines = 0
-
--- Use relative line numbers by default
-vim.o.relativenumber = true
-
--- Static word wrap
--- "vim.o.formatoptions+=cqt
-vim.o.textwidth = 80
-vim.o.linebreak = true
-
--- Indent behaviour
--- Default tabs are 4 spaces but don't expand tabs
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-
--- Hide buffer instead of closing it. Avoid to save the file when :e is called
-vim.o.hidden = true
-
--- Flash instead of beep
-vim.o.visualbell = true
---
--- Fast mode in tty
-vim.o.ttyfast = true
-
--- Global %s by default
-vim.o.gdefault = true
-
--- English as default language
-vim.o.spelllang = 'en'
+vim.o.fileformats = 'unix,dos' -- Unix based file format, only LF at end line
+vim.o.smartindent = true       -- Do smart autoindenting when starting a new line
+vim.o.ignorecase = true        -- Ignore case in search patterns
+vim.o.smartcase = true         -- ...unless upercase found
+vim.o.ttimeoutlen = 0          -- Time to wait for a key code sequence to complete
+vim.o.showcmd = true           -- Show (partial) command in bottom of the screen
+vim.o.scrolloff = 5            -- Number of lines above and below the cursor
+vim.o.autoread = true          -- Reload files changed outside vim
+vim.o.history = 1000           -- Long command history
+vim.o.splitright = true        -- Split in a more natural way
+vim.o.incsearch = true         -- Show pattern matches while typing
+vim.o.hlsearch = true          -- Highlight matches
+vim.o.showmatch = true         -- Match brackets briefly
+vim.o.modelines = 0            -- Avoid modelines exploits
+vim.o.relativenumber = true    -- Use relative line numbers by default
+vim.o.textwidth = 80           -- The 80 column police was here 🚓
+vim.o.linebreak = true         -- Static line beak
+vim.o.tabstop = 2              -- Tabs are counted as 2 spaces
+vim.o.softtabstop = 2          -- pressing tab will insert 2 spaces
+vim.o.expandtab = true         -- Complementary with softtabstop
+vim.o.shiftwidth = 2           -- autoindent will insert 2 spaces
+vim.o.hidden = true            -- Hide buffer instead of closing it.
+vim.o.visualbell = true        -- Flash instead of beep
+vim.o.ttyfast = true           -- Fast mode in tty
+vim.o.gdefault = true          -- Global %s by default
+vim.o.spelllang = 'en'         -- English as default language
